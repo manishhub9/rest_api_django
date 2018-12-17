3e14df84b32b92ea7ac57022289a4500ccc47faf
sanukyadav <sanu.yadav@go-mmt.com>
2018-12-17 17:34:22 +0530

    added profile api permissions and user creation token

--- src/profile_project/profiles_api/views.py
+++ src/profile_project/profiles_api/views.py
@@ -3,8 +3,11 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from . import serializers
+from rest_framework.authentication import TokenAuthentication
 
+from . import serializers
+from . import models 
+from . import permissions 
 
 # Create your views here.
 
@@ -93,3 +96,11 @@ class HelloViewSet(viewsets.ViewSet):
 		""" Handles removing  an object """
 		return Response({'hhtp_methos': 'DELETE'}) 		
 
+class UserProfileViewSet(viewsets.ModelViewSet):
+	""" Handles creating, creating and updating profiles"""
+
+	serializer_class = serializers.UserProfileSerializer
+	queryset = models.UserProfile.objects.all()
+	authentication_classes = (TokenAuthentication,)
+	permission_classes = (permissions.UpdateOwnProfile,)
+	

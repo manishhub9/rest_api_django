ae87dea5d41de0da5393cdc07736a89ac494cdfc
sanukyadav <sanu.yadav@go-mmt.com>
2018-12-17 18:27:19 +0530

    added authToken for user login

--- src/profile_project/profiles_api/views.py
+++ src/profile_project/profiles_api/views.py
@@ -4,6 +4,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+
 
 from . import serializers
 from . import models 
@@ -103,4 +107,16 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 	queryset = models.UserProfile.objects.all()
 	authentication_classes = (TokenAuthentication,)
 	permission_classes = (permissions.UpdateOwnProfile,)
-	
+	filter_backends = (filters.SearchFilter,)
+	search_fields = ('name', 'email',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+	"""Checks email and password and returns an auth token."""
+
+	serializer_class = AuthTokenSerializer
+
+	def create(self, request):
+		"""Use the ObtainAuthToken APIView to validate and create a token."""
+
+		return ObtainAuthToken().post(request)
